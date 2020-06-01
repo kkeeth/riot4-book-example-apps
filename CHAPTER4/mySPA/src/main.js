c6058b45b64aa8ba8a41fb617d1b7f3fdf5a1d2e
kkeeth <zensin0082@gmail.com>
2020-06-01 17:30:31 +0900

    feat: add parcel temlate

---
+++ CHAPTER4/mySPA/src/main.js
@@ -0,0 +1,8 @@
+import '@riotjs/hot-reload'
+import App from './app.riot'
+import {component} from 'riot'
+
+// これでコンポーネントをグローバルに登録とマウントを同時に行っています
+component(App)(document.getElementById('app'), {
+  title: 'Hello Riot.js!'
+})
