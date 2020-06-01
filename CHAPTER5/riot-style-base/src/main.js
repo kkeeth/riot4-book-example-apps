0a428113a9fd6e82e7567eeec314d1224ff00550
kkeeth <zensin0082@gmail.com>
2020-06-02 02:55:20 +0900

    feat: added subtitle with coloring about style base

--- CHAPTER5/riot-style-base/src/main.js
+++ CHAPTER5/riot-style-base/src/main.js
@@ -3,5 +3,6 @@ import App from './app.riot'
 import {component} from 'riot'
 
 component(App)(document.getElementById('app'), {
-  title: 'Hello Riot.js!'
+  title: 'Hello Riot.js!',
+  subTitle: 'This is parent👨'
 })
