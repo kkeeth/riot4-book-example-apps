ef2ba1be6ee4fdcfcf980fee4224292a31fb4d70
kkeeth <zensin0082@gmail.com>
2022-08-04 08:53:49 +0900

    fix: tweak chapter3 of complete

--- CHAPTER3/complete/observable.js
+++ CHAPTER3/complete/observable.js
@@ -1,81 +1,90 @@
-;(function(window, undefined) {const ALL_CALLBACKS = '*'
-const define = Object.defineProperties
-const entries = Object.entries
+;(function (window, undefined) {
+  const ALL_CALLBACKS = "*";
+  const define = Object.defineProperties;
+  const entries = Object.entries;
 
-const on = (callbacks, el) => (event, fn) => {
-  if (callbacks.has(event)) {
-    callbacks.get(event).add(fn)
-  } else {
-    callbacks.set(event, new Set().add(fn))
-  }
+  const on = (callbacks, el) => (event, fn) => {
+    if (callbacks.has(event)) {
+      callbacks.get(event).add(fn);
+    } else {
+      callbacks.set(event, new Set().add(fn));
+    }
 
-  return el
-}
+    return el;
+  };
 
-const off = (callbacks, el) => (event, fn) => {
-  if (event === ALL_CALLBACKS && !fn) {
-    callbacks.clear()
-  } else {
+  const deleteCallback = (callbacks, el, event, fn) => {
     if (fn) {
-      const fns = callbacks.get(event)
+      const fns = callbacks.get(event);
 
       if (fns) {
-        fns.delete(fn)
-        if (fns.size === 0) callbacks.delete(event)
+        fns.delete(fn);
+        if (fns.size === 0) callbacks.delete(event);
       }
-    } else callbacks.delete(event)
-  }
-  return el
-}
+    } else callbacks.delete(event);
+  };
 
-const one = (callbacks, el) => (event, fn) => {
-  function on(...args) {
-    el.off(event, on)
-    fn.apply(el, args)
-  }
-  return el.on(event, on)
-}
+  const off = (callbacks, el) => (event, fn) => {
+    if (event === ALL_CALLBACKS && !fn) {
+      callbacks.clear();
+    } else {
+      deleteCallback(callbacks, el, event, fn);
+    }
 
-const trigger = (callbacks, el) => (event, ...args) => {
-  const fns = callbacks.get(event)
+    return el;
+  };
 
-  if (fns) fns.forEach(fn => fn.apply(el, args))
+  const one = (callbacks, el) => (event, fn) => {
+    function on(...args) {
+      el.off(event, on);
+      fn.apply(el, args);
+    }
+    return el.on(event, on);
+  };
 
-  if (callbacks.get(ALL_CALLBACKS) && event !== ALL_CALLBACKS) {
-    el.trigger(ALL_CALLBACKS, event, ...args)
-  }
+  const trigger =
+    (callbacks, el) =>
+    (event, ...args) => {
+      const fns = callbacks.get(event);
 
-  return el
-}
+      if (fns) fns.forEach((fn) => fn.apply(el, args));
 
-const observable = function(el) { // eslint-disable-line
-  const callbacks = new Map()
-  const methods = {on, off, one, trigger}
-
-  el = el || {}
-
-  define(el,
-    entries(methods).reduce((acc, [key, method]) => {
-      acc[key] = {
-        value: method(callbacks, el),
-        enumerable: false,
-        writable: false,
-        configurable: false
+      if (callbacks.get(ALL_CALLBACKS) && event !== ALL_CALLBACKS) {
+        el.trigger(ALL_CALLBACKS, event, ...args);
       }
 
-      return acc
-    }, {})
-  )
+      return el;
+    };
 
-  return el
-}
+  const observable = function (el) {
+    // eslint-disable-line
+    const callbacks = new Map();
+    const methods = { on, off, one, trigger };
+
+    el = el || {};
+
+    define(
+      el,
+      entries(methods).reduce((acc, [key, method]) => {
+        acc[key] = {
+          value: method(callbacks, el),
+          enumerable: false,
+          writable: false,
+          configurable: false,
+        };
+
+        return acc;
+      }, {})
+    );
+
+    return el;
+  };
   /* istanbul ignore next */
   // support CommonJS, AMD & browser
-  if (typeof exports === 'object')
-    module.exports = observable
-  else if (typeof define === 'function' && define.amd)
-    define(function() { return observable })
-  else
-    window.observable = observable
-
-})(typeof window != 'undefined' ? window : undefined);
+  if (typeof exports === "object") module.exports = observable;
+  else if (typeof define === "function" && define.amd)
+    define(function () {
+      return observable;
+    });
+  else window.observable = observable;
+})(typeof window != "undefined" ? window : undefined);
